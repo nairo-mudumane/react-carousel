76b21c749f321e15e8fabe50f13e32944fd41ab8
NairoMudumane <nairomudumane@outlook.com>
2021-09-29 16:14:53 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css';
+import CarouselComponent from './components/CarouselComponent';
+
+export default function App() {
+  return (
+    <div className="App">
+      <CarouselComponent />
+    </div>
+  );
+}
